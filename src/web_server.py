e5b74f89df131319c9a171440ad90f71bb8040a6
JEONINSUCK <wdt0818@naver.com>
2023-07-13 10:45:08 +0900

    sync

--- src/web_server.py
+++ src/web_server.py
@@ -47,8 +47,17 @@ def bodyCheck(body_request):
     except Exception as err:
         print(f"bodyCheck error: {err}")
 
+def getMsgTheme(body: dict):
+    return body['message']['attachments'][0]['blocks'][1]['fields'][0]['text'].replace('*Theme:*',"")
+
 def getMsgTitle(body: dict):
-    return body['message']['attachments'][0]['blocks'][1]['fields'][1]['text'].replace('*Title:',"")
+    return body['message']['attachments'][0]['blocks'][1]['fields'][1]['text'].replace('*Title:*',"")
+
+def getMsgDate(body: dict):
+    return body['message']['attachments'][0]['blocks'][1]['fields'][2]['text'].replace('*Date:*',"")
+
+def getMsgTime(body: dict):
+    return body['message']['attachments'][0]['blocks'][1]['fields'][3]['text'].replace('*Time:*',"")
 
 def getActVal(body: dict):
     return body['actions'][0]['value']
@@ -65,7 +74,7 @@ async def getBody(request: Request):
     # get body from webhook request
     parse_request_body = await request.body()
     # convert bytes data to string data and decode
-    parse_request_body= parse.unquote(parse_request_body.decode('utf-8')).replace("payload=", "")
+    parse_request_body= parse.unquote(parse_request_body.decode('utf-8')).replace("payload=", "").replace("+", " ")
     # convert str to json type
     parse_request_body = json.loads(parse_request_body)
 
@@ -100,7 +109,10 @@ async def webhook(request: Request):
                 act_val = getActVal(body)
                 if act_val == config['CONF']['APPROVE_ACT_VAL']:
                     print("approve button")
+                    print(getMsgTheme(body))
                     print(getMsgTitle(body))
+                    print(getMsgDate(body))
+                    print(getMsgTime(body))
                 elif act_val == config['CONF']['DENY_ACT_VAL']:
                     print("deny button")
                 elif act_val == config['CONF']['INPUT']:
