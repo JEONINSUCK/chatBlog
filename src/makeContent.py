a7b2babfd3743cdaed014d6ea7c63829ef956752
JEONINSUCK <wdt0818@naver.com>
2023-08-02 00:12:47 +0900

    change qeury language english to korea and blog contents improve

--- src/makeContent.py
+++ src/makeContent.py
@@ -14,18 +14,24 @@ import re
 EXCHANGE_RATE = 1200
 DEFUALT_TOKEN = 1000
 
-CATEGORY_QUERY_BASE = "{0}에 관한 블로그 게시글을 작성할거야. {1}을 주제로한 카테고리 제목을 5가지 나열해 줘."
-CONTENT_QUERY_BASE = "{0}에 관한 블로그 게시글을 작성할거야. {1}을 주제로 리스트 형식의 글을 작성해 줘."
-SYSTEM_QUERY_BASE = "에 관한 블로거야."
+ASSIST_QUERY_BASE = "다음 입력될 내용은 블로그 게시글이야. 게시글이 입력되면 부족한 부분을 피드백 해줘"
+CATEGORY_QUERY_BASE = "{0}에 관한 블로그 게시글을 작성할거야. {1}을 주제로한 블로그 제목을 15글자 이하로 5가지 추천해 줘. 호기심을 자극하는 제목으로 부탁해. 20대 상냥한 여자 말투로 말해줘. 이모티콘도 추가해줘."
+CONTENT_QUERY_BASE = "{0}에 관한 블로그 게시글을 작성할거야. {1}을 주제로한 블로그 글을 작성해 줘. 20대 친근한 여성의 말투로 대답해 줘"
+SYSTEM_QUERY_BASE = "{0}에 관한 전문 블로거야."
 SYSTEM_CONTENT_BASE = "You are a helpful assistant who is good at detailing."
+ADV_QUERY_BASE = "피드백 받은 것을 바탕으로 위 게시글을 다시 작성해줘."
 
 # load config.json data
 with open("./config.json", "r", encoding="utf-8-sig") as f:
     config = json.load(f)
 
-# set API key & GPT model
+# # set API key & GPT model
 open_ai_key = config['AUTH']['GPT3_API_KEY']
 model = config['CONF']['GPT3_MODLE']
+
+# gpt-4 model setup
+# open_ai_key = config['AUTH']['GPT4_API_KEY']
+# model = config['CONF']['GPT4_MODLE']
 
 
 class makeContent:
@@ -46,29 +52,52 @@ class makeContent:
                 system_content = SYSTEM_CONTENT_BASE
             else:
                 system_content = system
+            
+            # conv_system = self.convModule.convEN(system_content).text
+            # conv_query = self.convModule.convEN(query).text
+            conv_system = system_content
+            conv_query = query
 
-            conv_query = self.convModule.convEN(query).text
-            messages = [
-                {
-                    "role": "system",
-                    "content": system_content
-                },
-                {
-                    "role": "user",
-                    "content": conv_query
-                },
-            ]
+            if assistant == "":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": conv_system
+                    },
+                    {
+                        "role": "user",
+                        "content": conv_query
+                    },
+                ]
+            else:
+                # conv_assistant = self.convModule.convEN(assistant).text
+                conv_assistant = assistant
+                messages = [
+                    {
+                        "role": "system",
+                        "content": conv_system
+                    },
+                    {
+                        "role": "user",
+                        "content": conv_assistant
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "위 내용에 대해 부족한 부분을 피드백 해줘."
+                    }
+                ]
 
             # query to chatGPT model
             response = openai.ChatCompletion.create(
                 model = model,
-                messages = messages
+                messages = messages,
+                temperature = 0.5
             )
 
             if 'choices' in response:
                 # parse answer and translate
-                answer = response['choices'][0]['message']['content']
-                conv_answer = self.convModule.convKO(answer).text
+                conv_answer = response['choices'][0]['message']['content']
+                # conv_answer = self.convModule.convKO(answer).text
 
                 token_num = self.tokenTool.getTokenNum(conv_query)
                 token_price = self.tokenTool.calcTokenPrice(token_num)
@@ -98,7 +127,7 @@ class makeContent:
             query = CATEGORY_QUERY_BASE.format(self.theme, self.theme)
             
             try:
-                response = self.querySend(query)
+                response = self.querySend(query, system=self.theme)
             except Exception as e:
                 debugPrint("[-] Query send FAIL")
                 return ERRORCODE._QUERY_FAIL
@@ -148,23 +177,33 @@ class makeContent:
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
             
-            # query = "초보자에 관련된 운동 블로그를 써줘. 가슴 운동에 관한 운동 방법에 대해 써줘. 제일 처음 제목을 써줘. 다른 운동 블로그를 참고 하여 전문적인 표현을 많이 사용해 줘"
             self.query = CONTENT_QUERY_BASE.format(self.theme, query_string)
-            # self.conv_query = self.convModule.convEN(self.query).text
             
             try:
-                self.conv_answer = self.querySend(self.query)
+                # main query
+                main_answer = self.querySend(self.query, system=SYSTEM_QUERY_BASE.format(self.theme))
+                # feedback query
+                # assist_answer = self.querySend(ASSIST_QUERY_BASE, system=SYSTEM_QUERY_BASE.format(self.theme), assistant=main_answer['response'])
+                # advanced query
+                # adv_answer = self.querySend(ADV_QUERY_BASE, system=SYSTEM_QUERY_BASE.format(self.theme), assistant=self.conv_answer['response']+assist_answer['response'])
             except Exception as e:
                 debugPrint("[-] Query send FAIL")
                 return ERRORCODE._QUERY_FAIL
-            self.parse_answer = self.conv_answer['response'].split('\n')
+            # self.parse_answer = main_answer['response'].split('\n')
 
             # write answer to file
             with open(file_path, 'w') as f:
-                f.write(self.conv_answer['response'])
+                f.write(main_answer['response'])
+            # adv_file_path = os.path.join(*[config['CONF']['MEMORY_PATH'], config['CONF']['CONTENTS_PATH'], query_string+"_adv"])
+            # with open(adv_file_path, 'w') as f:
+            #     f.write(assist_answer['response'])
+                
 
             debugPrint("[+] Make content Ok...")
-            return self.conv_answer
+            # self.conv_answer['token'] += assist_answer['token']
+            # self.conv_answer['price'] += assist_answer['price']
+            # return self.conv_answer
+            return main_answer
         
         except Exception as e:
             debugPrint("[-] Make content FAIL...")
@@ -306,12 +345,15 @@ if __name__ == '__main__':
     test_tokenTool = tokenUtility()    
     
     test_makeContent.setTheme("헬스")
-    test_makeContent.makeCategory()
-    # title = test_makeContent.getTitleSrc("헬스")
-    # print(title)
-    # test_makeContent.makeContent(title)
+    # test_makeContent.makeCategory()
+    title = test_makeContent.getTitleSrc("헬스")
+    print(title)
+    print(test_makeContent.makeContent(title)['response'])
     # for theme in test_makeContent.getThemeSrc():
     #     if test_makeContent.getTitleSrc(theme) == ERRORCODE._TITLE_USED:
     #         print("not exist using title")
+
+    # print(test_makeContent.querySend("블로그 글을 잘쓰는 법 5가지를 나열해 줘. 각각 15글자 이하로 간결하게 알려 줘.", system="전문적인 지식을 전달하는 것"))
+    # print(test_makeContent.querySend("'블로그 글을 잘쓰는 법'에 대해 구글에 검색하고 상위 5개 페이지 내용을 50자 이하로 요약해 줘.", system="전문적인 지식을 전달하는 것"))
     
     
